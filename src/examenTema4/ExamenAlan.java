7d06ac22be7eb1f70c721df2c842a36fdd12b91b
Alan <alangallardoclemente@hotmail.com>
2018-05-25 09:51:05 +0200

    Commit Alan

--- src/examenTema4/ExamenAlan.java
+++ src/examenTema4/ExamenAlan.java
@@ -1,13 +1,26 @@
 package examenTema4;
 
+import java.util.Date;
+
 public class ExamenAlan
 {
 	// Atributos
 	
 	private int nombre;
+	private Date fechaExamen;
 
 	// Getters & Setters
 	
+	public Date getFechaExamen()
+	{
+		return fechaExamen;
+	}
+
+	public void setFechaExamen(Date fechaExamen)
+	{
+		this.fechaExamen = fechaExamen;
+	}
+
 	public int getNombre()
 	{
 		return nombre;
