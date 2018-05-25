e732b59222d94b019e25f2b7344102e4b52d36f7
Ali <alisoultana@hotmail.com>
2018-05-25 10:21:12 +0200

    cambio master

--- src/Examen_SaimoukeAli.java
+++ src/Examen_SaimoukeAli.java
@@ -1,20 +1,42 @@
-package examenTema4;
 
 public class Examen_SaimoukeAli {
 	
 	//Atributo
-	int nombre;
+	private int nombre;
+	private Date fechaExamen;
+	private String descripcion;
 
 	
 	
 	
-	//Getters y Setters
+	//Getters 
 	public int getNombre() {
 		return nombre;
 	}
+	
+	public Date getfechaExamen() {
+		return fechaExamen;
+	}
+	public String getDescripcion()
+	{
+		return descripcion;
+	}
+	
+	
 
+	
+	//Setters
 	public void setNombre(int nombre) {
 		this.nombre = nombre;
+	}
+	
+	public void setfechaExamen(Date fechaExamen) {
+		this.fechaExamen = fechaExamen;
+	}
+	
+	public void setDescripcion(String descripcion)
+	{
+		this.descripcion = descripcion;
 	}
 	
 	
