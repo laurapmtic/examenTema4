7c1e2a11f31673dee4d05844256b845de87feb53
Miguel Santiago <lalleloschool@gmail.com>
2018-05-25 09:47:17 +0200

    Miguel Santiago 2

--- src/examenTema4/ExamenMiguelSantiago.java
+++ src/examenTema4/ExamenMiguelSantiago.java
@@ -1,15 +1,38 @@
 package examenTema4;
+import java.sql.Date;
 
 public class ExamenMiguelSantiago 
 {
 	int nombre = 0;
+	Date fechaExamen;
+	String descripcion;
 
+	//Set and get nombre
 	public int getNombre() {
 		return nombre;
 	}
 
 	public void setNombre(int nombre) {
 		this.nombre = nombre;
+	}
+
+	//Set and get fecha
+	public Date getFecha() {
+		return fechaExamen;
+	}
+
+	public void setFecha(Date Fecha) {
+		this.fechaExamen = Fecha;
+	}
+	
+	//Set and get descripcion
+	public String getDescripcion()
+	{
+		return descripcion;
+	}
+	public void setDescripcion(String des)
+	{
+		descripcion = des;
 	}
 	
 	
