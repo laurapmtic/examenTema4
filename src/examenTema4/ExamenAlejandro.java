0cf5c8729c6bc383d43f0dc0dd34908c643f6a12
dididothat <alems.info@gmail.com>
2018-05-25 09:27:33 +0200

    commit_descrp_master

--- src/examenTema4/ExamenAlejandro.java
+++ src/examenTema4/ExamenAlejandro.java
@@ -7,16 +7,28 @@ public class ExamenAlejandro
 {
 	//Atributo
 	int nombre;
-
+	String descripcion;
+	
 	//getter
 	public int getNombre()
 	{
 		return nombre;
 	}
+	
+	public String getDescripcion()
+	{
+		return descripcion;
+	}
+	
 	//setter
 	public void setNombre(int nom)
 	{
 		nombre = nom;
+	}
+	
+	public void setDescripcion(String descrp)
+	{
+		descripcion = descrp;
 	}
 	
 	
