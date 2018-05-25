d9acda762e9e01c12c284f47eb0efe67e7c9f4f6
Fran <fperez.ma@icloud.com>
2018-05-25 10:09:29 +0200

    ejercicio 7 francisco perez

--- src/examenTema4/ExamenFRANCISCO.java
+++ src/examenTema4/ExamenFRANCISCO.java
@@ -1,5 +1,25 @@
 package examenTema4;
 
+import java.util.Date;
+
 public class ExamenFRANCISCO {
+	int nombre;
+	Date fechaExamen;
+
+	public Date getFechaExamen() {
+		return fechaExamen;
+	}
+
+	public void setFechaExamen(Date fechaExamen) {
+		this.fechaExamen = fechaExamen;
+	}
+
+	public int getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(int nombre) {
+		this.nombre = nombre;
+	}
 
 }
