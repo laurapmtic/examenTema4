6919fbade0b94dbe50dddb901c4d6edf3b83c165
Fran <fperez.ma@icloud.com>
2018-05-25 10:22:54 +0200

    ejercicio 10 francisco perez

--- src/examenTema4/ExamenFRANCISCO.java
+++ src/examenTema4/ExamenFRANCISCO.java
@@ -1,5 +1,23 @@
 package examenTema4;
 
 public class ExamenFRANCISCO {
+	int nombre;
+	String descripcion;
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public int getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(int nombre) {
+		this.nombre = nombre;
+	}
 
 }
