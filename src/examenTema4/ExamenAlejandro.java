1ff0a549a7c11b9ab14d2ad9c86fcfd725f8d1b3
dididothat <alems.info@gmail.com>
2018-05-25 09:23:29 +0200

    commit_rama_ale

--- src/examenTema4/ExamenAlejandro.java
+++ src/examenTema4/ExamenAlejandro.java
@@ -7,16 +7,27 @@ public class ExamenAlejandro
 {
 	//Atributo
 	int nombre;
+	Date fechaExamen;	
 
 	//getter
 	public int getNombre()
 	{
 		return nombre;
 	}
+	public Date getFechaExamen()
+	{
+		return fechaExamen;
+	}
+
 	//setter
 	public void setNombre(int nom)
 	{
 		nombre = nom;
+	}
+
+	public void setFechaExamen(Date fecha)
+	{
+		fechaExamen = fecha;
 	}
 	
 	
