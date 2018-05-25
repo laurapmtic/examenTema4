48c5d399c80e00c7f8f27c75200a4a2fbdf3c5cc
Aldobg <aldobuenogarcia@gmail.com>
2018-05-25 09:49:11 +0200

    aldo ej6

--- src/examenTema4/ExamenAldo.java
+++ src/examenTema4/ExamenAldo.java
@@ -1,8 +1,11 @@
 package examenTema4;
 
+import java.util.Date;
+
 public class ExamenAldo {
 
 	private int aldo;
+	private Date fechaExamen;
 	
 	
 	//getters and setter
@@ -10,9 +13,17 @@ public class ExamenAldo {
 	public int getAldo() {
 		return aldo;
 	}
+	public Date getFechaExamen() {
+		return fechaExamen;
+	}
+	
 
 	public void setAldo(int aldo) {
 		this.aldo = aldo;
+	}
+	public void setFechaExamen(Date fechaExamen)
+	{
+		this.fechaExamen = fechaExamen;
 	}
 	
 }
