ae64792971853f381ed3e0d4f197a2e210eccb85
Jose Antonio Gomez <gomez95.16@hotmail.com>
2018-05-25 09:34:48 +0200

    Clase2

--- src/examenTema4/ExamenJoseAntonio.java
+++ src/examenTema4/ExamenJoseAntonio.java
@@ -1,14 +1,22 @@
 package examenTema4;
 
+import java.util.Date;
+
 public class ExamenJoseAntonio 
 {
 	//Atributo
 	int nombre;
+	Date fechaExamen;
 
 	//Get
 	public int getNombre() 
 	{
 		return nombre;
+	}
+	
+	public Date getFechaExamen() 
+	{
+		return fechaExamen;
 	}
 
 	//Set
@@ -16,6 +24,13 @@ public class ExamenJoseAntonio
 	{
 		this.nombre = nombre;
 	}
+
+	public void setFechaExamen(Date fechaExamen) 
+	{
+		this.fechaExamen = fechaExamen;
+	}
+	
+	
 	
 	
 	
