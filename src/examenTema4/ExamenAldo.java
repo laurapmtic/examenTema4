31f6692f84ccec8e9bf45e6070583c57ab177ce3
Aldobg <aldobuenogarcia@gmail.com>
2018-05-25 10:06:59 +0200

    Aldo ej10

--- src/examenTema4/ExamenAldo.java
+++ src/examenTema4/ExamenAldo.java
@@ -3,6 +3,7 @@ package examenTema4;
 public class ExamenAldo {
 
 	private int aldo;
+	private String descripcion;
 	
 	
 	//getters and setter
@@ -10,9 +11,18 @@ public class ExamenAldo {
 	public int getAldo() {
 		return aldo;
 	}
+	
+	public String getDescripcion() {
+		return descripcion;
+	}
 
 	public void setAldo(int aldo) {
 		this.aldo = aldo;
+	}
+	
+	public void setDescripcion(String descripcion)
+	{
+		this.descripcion = descripcion;
 	}
 	
 }
