874fa332f85b22decc2495acd9a9e2a5a4d28ac5
abidin Bazeid <abidin_bma@hotmail.com>
2018-05-25 09:48:05 +0200

    Abidin

--- src/examenTema4/ExamenAbidin.java
+++ src/examenTema4/ExamenAbidin.java
@@ -1,5 +1,29 @@
 package examenTema4;
 
-public class ExamenAbidin {
+public class ExamenAbidin 
+{
+	
+	int nombre;
+	Date fechaExamen;	
 
+	// getter
+	public int getNombre()
+	{
+		return nombre;
+	}
+	public Date getFechaExamen()
+	{
+		return fechaExamen;
+	}
+
+	// setter
+	public void setNombre(int nombre)
+	{
+		this.nombre = nombre;
+	}
+
+	public void setFechaExamen(Date fecha)
+	{
+		this.fechaExamen = fecha;
+	}
 }
