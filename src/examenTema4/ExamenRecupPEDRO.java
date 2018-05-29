7df4038f3406c4b1b1b68c9e6e77a64905479035
Pedro <pedro.jgm17@gmail.com>
2018-05-29 09:51:04 +0200

    Cambios punto 5

---
+++ src/examenTema4/ExamenRecupPEDRO.java
@@ -0,0 +1,17 @@
+package examenTema4;
+
+import java.sql.Date;
+
+public class ExamenRecupPEDRO {
+	
+	private Date fechaExamen;
+	
+	public Date getFechaexm() {
+		return this.fechaExamen;
+	}
+	
+	public void setFechaexm(Date fecha) {
+		this.fechaExamen = fecha;
+	}
+	
+}
